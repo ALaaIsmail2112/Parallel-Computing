c5301306fa92c2bfdcfb7bbefe646f6045a17fca
Alaa Ismail <alaaismailmohamed144@gmail.com>
2025-12-22 20:42:42 +0200

    Add files via upload

---
+++ Task_Section_9.java
@@ -0,0 +1,20 @@
+//////////////  THREADPOOLEXECUTOR IN JAVA   ////////////////
+// ThreadPoolExecutor (
+//      int corePoolSize,
+//      int maximumPoolSize,
+//      long keepAliveTime,
+//      TimeUnit unit,
+//      BlockingQueue<Runnable> workQueue
+// )
+
+
+
+////////////// SCHEDULEDTHREADPOOLEXECUTOR   //////////////
+
+// schedule(Runnable command, long delay, TimeUnit unit)
+
+
+// scheduleAtFixedRate(Runnable command, long initialDelay, long period, TimeUnit unit)
+
+
+// scheduleWithFixedDelay(Runnable command, long initialDelay, long delay, TimeUnit unit)
