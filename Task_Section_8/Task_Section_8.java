06794e42990163fab1ebc245d5f461ca45b3b77a
Alaa Ismail <alaaismailmohamed144@gmail.com>
2025-12-22 20:41:39 +0200

    Add files via upload

---
+++ Task_Section_8/Task_Section_8.java
@@ -0,0 +1,25 @@
+
+
+//////////////////// SEMAPHORE ////////////////////
+
+
+// Semaphore semaphore = new Semaphore(3);
+
+// semaphore.acquire();
+// try {
+//     // Critical section code here
+// } finally {
+//     semaphore.release();
+// }
+
+
+
+////////////////// COMPARE AND SWAP ////////////////////
+
+// AtomicInteger X = new AtomicInteger(34);
+
+// int expectedValue = 34;
+// int newValue = 99;
+
+
+// boolean sucess = X.compareAndSet(expectedValue, newValue);
